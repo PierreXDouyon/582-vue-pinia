d62d4e6a52ce01d248b8d21163416fb7b73f9818
PierreXDouyon <2295472@edu.vaniercollege.qc.ca>
2023-08-29 11:41:49 -0400

    added publick path to vue.config.js (1)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/", //https://router.vuejs.org/guide/essentials/history-mode.html#Example-Server-Configurations
+  publicPath: "/", //Documnetation Here: https://router.vuejs.org/guide/essentials/history-mode.html#Example-Server-Configurations
 });
